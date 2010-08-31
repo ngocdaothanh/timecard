c0f17544c032b64e57cdbdf158d40ad7e0344f7b
Lan Tra <tra.thanhlan@gnt.com.vn>
2010-08-31 14:37:33 +0700

    Add empty Rails app (without Active Record and Prototype.js)

---
+++ admin/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Admin::Application.config.secret_token = '5a21221e63f295eb4b72fb0c0a796951efe0c88e7522378a605abcb225800a807ba84d00b5fe890c2841c5c865951fc8002085d17cbe6b5c6c8282ee67dd5357'
