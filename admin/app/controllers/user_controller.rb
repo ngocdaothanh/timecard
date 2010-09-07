5c79ba0ce039fe2a4f116f25baa99ddabd744fbe
Lan Tra <tra.thanhlan@gnt.com.vn>
2010-09-07 15:43:47 +0700

    Edit Users

--- admin/app/controllers/user_controller.rb
+++ admin/app/controllers/user_controller.rb
@@ -9,14 +9,14 @@ class UserController < ApplicationController
 
   def create
     # only administrator allows create an user account
-    if not session['is_admin']
+    if not is_admin_
       go_home
     end
 
     @groups = Group.all
     @users = User.all
 
-    if request.post? and
+    if request.post?
       if params[:user][:password] == params[:user][:cfm_password]
         User.create(:username     => params[:user][:username],
                     :name         => params[:user][:name],
@@ -30,15 +30,65 @@ class UserController < ApplicationController
   end
 
   def edit
-    if is_admin_
-      @user = User.find(params[:id])
-      if @user.nil?
+    if authenticate     # authentication
+      if request.post?
+        if is_admin_      # administrator authorization
+          @user = User.find(params[:user][:id])
+          if params[:user][:password] == params[:user][:cfm_password]
+            if params[:user][:password] == ''
+              @user.update_attributes(
+                  :username  => params[:user][:username],
+                  :name      => params[:user][:name],
+                  :group_id  => params[:user][:group_id],
+                  :manager_id  => params[:user][:manager_id],
+                  :disabled    => params[:user][:disabled])
+            else
+              puts "Truoc khi cap nhat : encrypt #{@user.encrypted_password}"
+              @user.update_attributes(
+                  :username  => params[:user][:username],
+                  :password  => params[:user][:password],
+                  :name      => params[:user][:name],
+                  :group_id  => params[:user][:group_id],
+                  :manager_id  => params[:user][:manager_id],
+                  :disabled    => params[:user][:disabled])
+               puts "Sau khi cap nhat : encrypt #{@user.encrypted_password}"
+            end
+            redirect_to user_index_path
+          else
+            @groups = Group.all
+            @users = User.all
+            @message = 'Password and confirm password are not matching'
+          end
+        else
+          if session['user_id'] == params[:user][:id]   # edit only your record
+            @user = User.find(params[:user][:id])
+            if params[:user][:password] == params[:user][:cfm_password]
+              if params[:user][:password] != ''
+                @user.update_attributes(:password => params[:user][:password])
+              end
+              redirect_to user_index_path
+            else
+              @message = 'Password and confirm password are not matching'
+            end
+          end
+        end
       else
-
+        if is_admin_      # administrator authorization
+          @groups = Group.all
+          @users = User.all
+          @user = User.find(params[:id])
+        else              # regular user authorization
+          if session['user_id'] == params[:id] # ok, you can edit your record
+            session['is_authorized'] = true
+            @user = User.find(params[:id])
+          else                                  # no, you can not edit others
+            session['is_authorized'] = false
+            @message = 'You can only edit your record'
+          end
+        end
       end
-      @message = ''
     else
-      @message = 'Only administrator has deleted permisson'
+      must_login
     end
   end
 
