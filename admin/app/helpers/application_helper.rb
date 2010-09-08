4b84964d67f8f622cf5a44e92bd27919f328ca7f
Lan Tra <tra.thanhlan@gnt.com.vn>
2010-09-08 15:41:24 +0700

    Fix CookieOverflow

--- admin/app/helpers/application_helper.rb
+++ admin/app/helpers/application_helper.rb
@@ -1,12 +1,11 @@
 module ApplicationHelper
   def current_user
-     #puts "Trong ApplicationHelper current_user"
-     if session['current_user'].nil?
-       nil
-     end
-     session['current_user']
+    unless @current_user
+      @current_user = User.find(session['user_id'])
+    end
+    @current_user
   end
-
+  
   def isManagerOf(user)
     manager_id = User.find(user.id).manager_id
 
