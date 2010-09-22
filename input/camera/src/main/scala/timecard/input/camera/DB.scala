b11d601465282d62ab921d6c3596be105053a271
Lan Tra <tra.thanhlan@gnt.com.vn>
2010-09-22 14:28:25 +0700

    Use Scala code to work with DB

--- input/camera/src/main/scala/timecard/input/camera/DB.scala
+++ input/camera/src/main/scala/timecard/input/camera/DB.scala
@@ -1,11 +1,12 @@
 package timecard.input.camera
 
-import java.util.Date
 import org.jcouchdb.db._
-import org.jcouchdb.document.BaseDocument
-import lantra.DBJava
-import java.io.File
-
+import org.jcouchdb.exception.NotFoundException
+import java.util._
+import org.jcouchdb.util.CouchDBUpdater
+import java.io.{IOException, File}
+import java.text.{SimpleDateFormat, DateFormat}
+import org.jcouchdb.document.{ViewAndDocumentsResult, BaseDocument}
 
 object DB {
   val HOST = "localhost"
@@ -18,27 +19,69 @@ object DB {
   if (!databases.contains(DB_NAME)) {
     server.createDatabase(DB_NAME)
   }
-  //val db = new Database(HOST, DB_NAME)
+  val db = new Database(HOST, DB_NAME)
 
-  val dbjava = new DBJava(HOST, DB_NAME)
-
-  dbjava.createMapReduceJS("timecard-views")
+  createMapReduceJS("timecard-views")
 
   def getName(userId: String): String = {
-    dbjava.getName(userId)
+    try {
+      val doc: Map[String, String] = db.getDocument(classOf[Map[String, String]], userId)
+      doc.get("name")
+    } catch {
+      case nfe: NotFoundException => {
+        null
+      }
+    }
   }
 
   def saveTimeEntry(user_id: String, timeOption:String, time:Date) {
-    //println("Trong saveTimeEntry : " + user_id  + ":" + timeOption + ":" + time)
-    dbjava.saveTimeEntry(user_id, timeOption, time)
+    val date: String = new SimpleDateFormat("yyyy-MM-dd").format(time)
+
+    //Find by user_id and date
+    val result = db.queryViewAndDocumentsByKeys("timeentry/find_by_userid_and_date", classOf[AnyRef],
+      classOf[Map[String, String]], Arrays.asList(Arrays.asList(user_id, date)), null, null)
+
+    //println(result.getRows().size());
+    if (result.getRows.size != 0) {
+      val doc: Map[String, String] = result.getRows.get(0).getDocument
+      doc.put(timeOption, timeToGMT(time))
+      db.createOrUpdateDocument(doc)
+    }
+    else {
+      val doc: Map[String, String] = new HashMap[String, String]
+      doc.put("user_id", user_id)
+      doc.put("date", date)
+      doc.put(timeOption, timeToGMT(time))
+      doc.put("ruby_class", "Timeentry")
+      db.createDocument(doc)
+    }
+  }
+
+  private def timeToGMT(time: Date):String = {
+    val tz: TimeZone = TimeZone.getTimeZone("GMT:00")
+    val dfGMT = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG)
+    dfGMT.setTimeZone(tz)
+    dfGMT.format(time)
+  }
+
+  def createMapReduceJS(viewDir: String) {
+    val updater = new CouchDBUpdater
+    updater.setDatabase(db)
+    val dir = new File(viewDir)
+    updater.setDesignDocumentDir(dir)
+    try {
+      updater.updateDesignDocuments
+    } catch {
+      case e: IOException => {
+        e.printStackTrace
+      }
+    }
   }
 
   /*
   def main(args: Array[String]) {
-    //println(getName("ae5e44340c2a0f3e3f68a785ca4eb3944"))
+    //println(getName("ae5e44340c2a0f3e3f68a785ca4eb394"))
     //saveTimeEntry("","",new java.util.Date)
-    val f = new File("timecard-views")
-    println(f.getAbsoluteFile)
-  }
-  */
+  } */
+
 }
